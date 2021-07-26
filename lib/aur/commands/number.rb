e811387e5970b00919d758a82f1d6291dc73b088
Robert Fisher <services@id264.net>
2021-07-26 11:24:24 +0000

    make number command work, and test it

--- lib/aur/commands/number.rb
+++ lib/aur/commands/number.rb
@@ -3,64 +3,47 @@
 require_relative 'base'
 require_relative '../constants'
 require_relative '../tag_edit'
+require_relative '../fileinfo'
 require_relative '../stdlib/string'
 
 module Aur
-  module Name2tag
+  module Number
     #
-    # Retag a file from its name
+    # Set the track number tag from the file name.
     #
     class Generic < Aur::Base
-      TAGS = %i[artist title album t_num].freeze
       include Aur::TagEdit
 
       def run
-        puts file
-        tags = tags_from_filename
-        tags.each_pair { |k, v| tag_msg(k, v) }
-        apply_tags(tags)
-      end
+        track_number = info.f_t_num.to_i
 
-      private
-
-      # @return [Hash] tag_name => value
-      #
-      def tags_from_filename
-        TAGS.each_with_object({}) do |t, a|
-          a[info.send(:tag_for, t)] = mk_title(info.send("f_#{t}".to_sym))
+        if track_number.zero?
+          warn 'No number found at start of filename'
+          return
         end
-      end
 
-      def tag_msg(name, val)
         msg format('%12<tag_name>s -> %<tag_value>s',
-                   tag_name: name,
-                   tag_value: val)
+                   tag_name: 'track_number',
+                   tag_value: track_number)
+        apply_tag(track_number)
       end
     end
 
-    # Re-tag FLACs
+    # Set the track number of a FLAC
     #
     class Flac < Generic
-      def apply_tags(tags)
-        tags.each_pair do |k, v|
-          info.raw.comment_del(k.to_s.upcase)
-          info.raw.comment_add("#{k.upcase}=#{v}")
-        end
-
+      def apply_tag(track_number)
+        info.raw.comment_del('TRACKNUMBER')
+        info.raw.comment_add("TRACKNUMBER=#{track_number}")
         info.raw.update!
       end
     end
 
-    # Re-tag MP3s
+    # Set the track number of an MP3
     #
     class Mp3 < Generic
-      def apply_tags(tags)
-        Mp3Info.open(file) do |f|
-          f.tag.title = tags[:title]
-          f.tag.artist = tags[:artist]
-          f.tag.album = tags[:album]
-          f.tag.tracknum = tags[:tracknum].to_i
-        end
+      def apply_tag(track_number)
+        Mp3Info.open(file) { |f| f.tag.tracknum = track_number }
       end
     end
   end
---
+++ spec/aur/commands/number_functional_spec.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require_relative '../../spec_helper'
+require_relative '../../../lib/aur/command'
+
+# Run 'aur numname' commands against things, and verify the results
+#
+class TestNumberCommand < MiniTest::Test
+  def test_flac_number
+    setup_test_dir
+    source_file = TMP_DIR + '01.the_null_set.song_one.flac'
+    FileUtils.cp(RES_DIR + '01.the_null_set.song_one.flac', TMP_DIR)
+    assert(source_file.exist?)
+
+    out, = capture_io { Aur::Command.new(:info, [source_file]).run! }
+    refute_match(/Track no : 2/, out)
+
+    out, err = capture_io { Aur::Command.new(:number, [source_file]).run! }
+    assert_empty(err)
+    assert_equal('track_number -> 1', out.strip)
+
+    assert(source_file.exist?)
+
+    out, err = capture_io { Aur::Command.new(:info, [source_file]).run! }
+    assert_empty(err)
+    assert_match(/Track no : 1/, out)
+
+    cleanup_test_dir
+  end
+
+  def test_mp3_number
+    setup_test_dir
+    source_file = TMP_DIR + '01.the_null_set.song_one.mp3'
+    FileUtils.cp(RES_DIR + '01.the_null_set.song_one.mp3', TMP_DIR)
+    assert(source_file.exist?)
+
+    out, = capture_io { Aur::Command.new(:info, [source_file]).run! }
+    refute_match(/Track no : 2/, out)
+
+    out, err = capture_io { Aur::Command.new(:number, [source_file]).run! }
+    assert_empty(err)
+    assert_equal('track_number -> 1', out.strip)
+
+    assert(source_file.exist?)
+
+    out, err = capture_io { Aur::Command.new(:info, [source_file]).run! }
+    assert_empty(err)
+    assert_match(/Track no : 1/, out)
+
+    cleanup_test_dir
+  end
+end
