c7e5bf0c6fde2f50e8bb7cc49a4984530ea2eb3c
Robert Fisher <services@id264.net>
2021-08-18 10:20:35 +0000

    change "bump" command to "renumber"

--- lib/aur/commands/renumber.rb
+++ lib/aur/commands/renumber.rb
@@ -11,11 +11,14 @@ module Aur
     # Increments the track number tag, and the filename number, by the given
     # value. This value may be negative.
     #
-    class Bump < Base
+    class Renumber < Base
       include Aur::Renamers
 
       def run
         delta = validate(opts[:'<number>'])
+
+        delta = 0 - delta if opts[:down]
+
         new_number = calculate_new_number(delta)
         tagger.tag!(t_num: new_number)
         rename_file(file, renumbered_file(new_number))
@@ -29,20 +32,19 @@ module Aur
       end
 
       def validate(input)
-        return input.to_i if /^-?\d+$/.match?(input)
+        return input.to_i if /^\d+$/.match?(input) && input.to_i.positive?
 
         raise(Aur::Exception::InvalidValue, input)
       end
 
       def self.help
         <<~EOHELP
-          usage: aur bump <value> <file>...
+          usage: aur renumber (up|down) <value> <file>...
 
-          'bump' increments the track number of the given file(s) by the given
-          integer value. It changes the track number tag and the track number at
-          the beginning of the filename.
+          'renumber' changes the track number of the given file(s) by the given
+          integer value in the given direction. It modifies the track number
+          tag and the track number at the beginning of the filename.
 
-          Negative values can be given, which will decrement the track number.
           An error is thrown if a tag number drops below one.
         EOHELP
       end
--- lib/aur/constants.rb
+++ lib/aur/constants.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'pathname'
+
 SUPPORTED_TYPES = %w[flac mp3].freeze
 
 BIN = {
