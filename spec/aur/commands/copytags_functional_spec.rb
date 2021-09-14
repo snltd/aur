e5827f4f63e425cb687e8d64c5c0b2bf29a11e03
Robert Fisher <1950940+snltd@users.noreply.github.com>
2021-09-14 14:09:23 +0100

    add copytags command (#59)
    * add copytags command
    
    * fix tests
    
    Co-authored-by: Robert Fisher <services@id264.net>

---
+++ spec/aur/commands/copytags_functional_spec.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require_relative '../../spec_helper'
+require_relative '../../../lib/aur/action'
+require_relative '../../../lib/aur/fileinfo'
+
+# Run 'aur copytags' commands against things, and verify the results
+#
+class TestCopytagsCommand < MiniTest::Test
+  def test_remove_v1_tags_from_file_with_both
+    with_test_file('copytags') do |dir|
+      flac = dir + 'flac' + '01.artist.song.flac'
+      mp3 = dir + 'mp3' + '01.artist.song.mp3'
+
+      flacinfo = Aur::FileInfo.new(flac)
+      mp3info = Aur::FileInfo.new(mp3)
+
+      assert_equal({ artist: nil,
+                     album: nil,
+                     title: nil,
+                     t_num: nil,
+                     year: nil,
+                     genre: 'Noise' },
+                   mp3info.our_tags)
+
+      assert_equal(flac_tags, flacinfo.our_tags)
+
+      capture_io { Aur::Action.new(:copytags, [mp3]).run! }
+
+      assert_equal(flac_tags, Aur::FileInfo.new(mp3).our_tags)
+    end
+  end
+
+  def flac_tags
+    { artist: 'Artist',
+      album: 'Test Album',
+      title: 'Song',
+      t_num: '1',
+      year: '2021',
+      genre: 'Noise' }
+  end
+end
---
+++ spec/aur/commands/copytags_spec.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require_relative '../../spec_helper'
+
+# Done with functional spec
+#
+class TestCopytags < MiniTest::Test; end
