012ff8512bb982d381e5b408565083025e79ab97
Robert Fisher <services@id264.net>
2022-05-23 10:25:47 +0000

    more caps stuff

--- lib/aur/constants.rb
+++ lib/aur/constants.rb
@@ -35,14 +35,16 @@ ALL_CAPS = %w[ok dj lp ep l.a. bmr bbc scsi ii iii iv vi vii ix xi cd fm tko
               kb gr gb uk mdma tvr17 tvc wmc emi tda vsx lcd kg uv jlh bs ts
               ww3 wr fbi vvv tx qwerty nw abc gq olv sm57 wkrp hp hc ox4 jc qrj
               dvd tnt acd csi xr3 acac 4ad3dcd vip kfc uvtv nme gf bf abba bmx
-              fc 3cp a&r r&b ihop lsd lcd lp2 mbv pcp urc uk nato afx
-              tv gtg bnoc nypd mtv od afx2 brkn01 brkn02 brkn03].freeze
+              fc 3cp a&r r&b ihop lsd lcd lp2 mbv pcp urc uk nato afx mj ac/dc
+              jxl tm usm klf m/a/r/r/s mc5 ps tv gtg bnoc nypd mtv od afx2
+              mc brkn01 brkn02 brkn03].freeze
 
 # These words should be ignored when examining case
 #
 IGNORE_CASE = %w[d'isan o'keefe mcgee mcintyre ça b's mccay mccoy lps eps mxbx
                  mccarthy d'or a: iiia iiib 5-6-7-8's v/vm mccartney mms
-                 mcdonalds].freeze
+                 mcalmont mccarthy
+                 tatu d'arby mccombs mcdonalds].freeze
 
 # The keys of this hash are expanded to the corresponding value when
 # generating tags. Some of these contractions are, of course, real words, but
