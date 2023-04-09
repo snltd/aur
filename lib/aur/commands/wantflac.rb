2b1398c945df29d8610445d9909113e5fc313ace
Robert Fisher <services@id264.net>
2023-04-09 22:04:20 +0000

    add wantflac -T option to diff tracks dirs

--- lib/aur/commands/wantflac.rb
+++ lib/aur/commands/wantflac.rb
@@ -9,8 +9,25 @@ module Aur
     # Shows which things we have as MP3 but not as FLAC
     #
     class Wantflac < Syncflac
+      def run
+        if opts[:tracks]
+          track_difference
+        else
+          difference(flacs, mp3s).each { |dir| action(dir) }
+        end
+      end
+
       def action(dir)
         puts dir
+      end
+
+      def track_difference
+        flacs = flac_track_filenames
+
+        puts mdir.join('tracks').children.each_with_object([]) { |f, aggr|
+          bn = f.basename.to_s
+          aggr << bn unless flacs.key?(bn.sub(/mp3$/, 'flac'))
+        }.sort
       end
 
       def difference(flacdirs, mp3dirs)
@@ -36,8 +53,15 @@ module Aur
         <<~EOHELP
           usage: aur wantflac
 
-          Lists MP3 albums and EPs which we do not have as FLACs.
+          Lists MP3 albums and EPs which we do not have as FLACs. With the -t
+          flag, lists tracks.
         EOHELP
+      end
+
+      private
+
+      def flac_track_filenames
+        fdir.join('tracks').children.to_h { |f| [f.basename.to_s, true] }
       end
     end
   end
